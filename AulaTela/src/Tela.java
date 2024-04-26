ebc67ac511aa97ef4c6e3ab5e02961562f403ac5
NatieleCosta <natisouza806@gmail.com>
2024-04-26 19:02:35 -0300

    att

--- AulaTela/src/Tela.java
+++ AulaTela/src/Tela.java
@@ -1,4 +1,4 @@
-package aulatela;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
--- AulaTela/src/Teste.java
+++ AulaTela/src/Teste.java
@@ -1,4 +1,4 @@
-package aulatela;
+
 
 public class Teste {
     public static void main(String[] args) {
