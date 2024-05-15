88cc906492f7abbb146412c71ae51023a183fff8
Yan da Costa Fernandes Gomes <143640083+yancfgomes@users.noreply.github.com>
2024-05-15 09:33:47 -0400

    update in main code

--- AtividadeTela/src/main/menu.java
+++ AtividadeTela/src/main/menu.java
@@ -20,7 +20,7 @@ public class Menu {
 	// Criação da janela
         JFrame frame = new JFrame("Sistema Bibliotecário");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setSize(700,500);
        
      // Criação da barra de menu
         JMenuBar barramenu = new JMenuBar();
@@ -90,6 +90,8 @@ public class Menu {
         fileMenu.add(administrador);
         fileMenu.addSeparator(); // Adiciona uma linha separadora
         fileMenu.add(sair);
+
+      
         
      
        
