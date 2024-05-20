64968aa0207cf8ff2bb5090e384db9ebbee50896
NatieleCosta <natisouza806@gmail.com>
2024-05-20 19:11:51 -0300

    Atidades da DIO

---
+++ CursoJavaDeveloper/src/ArraysPares.java
@@ -0,0 +1,13 @@
+public class ArraysPares {
+    public static void main(String[] args){
+        int[] array = { 2, 3, 5, 7, 11, 13, 18, 34};
+        @SuppressWarnings("unused")
+        int tamanho = array.length;
+
+        for(int j : array) {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+    }
+}
