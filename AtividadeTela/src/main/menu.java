5d21103461c164a3e78436594ef4c82b8c0a71c1
adizairaa <82561744+adizairaa@users.noreply.github.com>
2024-05-21 13:14:25 -0400

    Update menuad.java
    Adicionei o ícone no menu de Acervo de livros

--- AtividadeTela/src/main/menu.java
+++ AtividadeTela/src/main/menu.java
@@ -1,109 +1,157 @@
 package main;
-import java.awt.Color;
-import java.awt.Frame;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import logic.Biblioteca;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import ui.InterfaceAdministrador;
 import ui.InterfaceBibliotecario;
 import ui.InterfaceMembroBiblioteca;
-import ui.InterfaceAdministrador;
+import logic.AcervoDeLivros;
+import logic.Biblioteca;
 
+public class UpdateMenu extends JPanel {
+    private BufferedImage backgroundImage;
 
-public class Menu {
+    public UpdateMenu() {
+        Biblioteca biblioteca = new Biblioteca();
 
-	public static void main(String[] args) {
-		Biblioteca biblioteca = new Biblioteca();
-		
-	// Criação da janela
-        JFrame frame = new JFrame("Sistema Bibliotecário");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
-       
-     // Criação da barra de menu
+        // Carrega a imagem de fundo
+        try {
+            backgroundImage = ImageIO.read(new File("estudos-java/AtividadeTela/src/main/background.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Criação da barra de menu
         JMenuBar barramenu = new JMenuBar();
-      
-     // Criação dos menus
+
+        // Criação dos menus
         JMenu fileMenu = new JMenu("Menu");
-        JMenu aboutMenu = new JMenu("Sobre");        
-        
-     // Criação dos itens de menu para o menu 
+        JMenu aboutMenu = new JMenu("Sobre");
+        JMenu collectioMenu = new JMenu("Acervo de livros");
+
+        // Criação dos itens de menu para o menu
         JMenuItem bibliotecario = new JMenuItem("Bibliotecário");
         JMenuItem membro = new JMenuItem("Membro");
         JMenuItem administrador = new JMenuItem("Administrador");
         JMenuItem sair = new JMenuItem("Sair");
-        
-     // Adiciona um ouvinte de ação ao item "Sair"
+        JMenuItem acervoDeLivros = new JMenuItem("Acervo de Livros"); // Corrigido: Inicialização do item de menu
+
+        // Criar o item de menu "Sobre"
+        JMenuItem sobre = new JMenuItem("Sobre");
+        aboutMenu.add(sobre);
+
+        // Adicionar um ActionListener para o item de menu "Sobre"
+        sobre.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                    "Sistema Bibliotecário\n Atividade de seminario da matéria de POO do 3º periodo do curso de ADS \n Sistema de gerenciamento de biblioteca desenvolvido em Java, com uma interface gráfica de usuário (GUI).\n \n\nProgramadores:\n- Adila \n- Yan \n- Natiele ",
+                    "Sobre",
+                    JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        // Adiciona um ouvinte de ação ao item "Sair"
         sair.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Exibe uma caixa de diálogo de confirmação ao clicar em "Sair"
-                int option = JOptionPane.showConfirmDialog(frame, "Deseja sair?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                int option = JOptionPane.showConfirmDialog(null, "Deseja sair?", "Confirmação", JOptionPane.YES_NO_OPTION);
                 if (option == JOptionPane.YES_OPTION) {
                     System.exit(0); // Sai do programa
                 }
             }
-        }); 
-        // ações ao clicar no menuitem bibliotecario
+        });
+
+        // Ações ao clicar no menuitem bibliotecario
         bibliotecario.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				InterfaceBibliotecario interfaceBibliotecario = new InterfaceBibliotecario(biblioteca);
-				interfaceBibliotecario.setVisible(true);
-			}
-		}); 
-        // ações ao clicar no menuitem membro
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InterfaceBibliotecario interfaceBibliotecario = new InterfaceBibliotecario(biblioteca);
+                interfaceBibliotecario.setVisible(true);
+            }
+        });
+
+        // Ações ao clicar no menu item membro
         membro.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				InterfaceMembroBiblioteca interfaceMembroBiblioteca = new InterfaceMembroBiblioteca(biblioteca);
-				interfaceMembroBiblioteca.setVisible(true);
-				
-			}
-		});
-        // ações ao clicar no menuitem Administrador
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InterfaceMembroBiblioteca interfaceMembroBiblioteca = new InterfaceMembroBiblioteca(biblioteca);
+                interfaceMembroBiblioteca.setVisible(true);
+            }
+        });
+
+        // Ações ao clicar no menu item Administrador
         administrador.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				InterfaceAdministrador interfaceAdministrador = new InterfaceAdministrador(biblioteca);
-				interfaceAdministrador.setVisible(true);	
-			}
-		});
-     
-         
-     // Define a janela como visível
-        frame.setVisible(true);
-     
-    // Define a barra de menu para a janela
-        frame.setJMenuBar(barramenu); 
-        
-        
-     // Adiciona os menus à barra de menu
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InterfaceAdministrador interfaceAdministrador = new InterfaceAdministrador(biblioteca);
+                interfaceAdministrador.setVisible(true);
+            }
+        });
+
+        // Ações ao clicar no menuitem Acervo de Livros
+        acervoDeLivros.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Criar lista de livros de exemplo
+                List<String[]> livros = new ArrayList<>();
+                livros.add(new String[]{"1", "JAVA como programar ", "Paul Deitel", "2016", "Disponível"});
+                livros.add(new String[]{"2", "Engenharia de Softwarw ", "Roger S. Pressman", "2021", "Emprestado"});
+                livros.add(new String[]{"3", "Sistemas de Banco de Dados", "Elmasri", "2019", "Disponível"});
+                livros.add(new String[]{"4", "Sistemas Operacionais Modernos", "Andrew S Tanenbaum", "2024", "Disponível"});
+                livros.add(new String[]{"5", "Entendendo Algoritmos: guia Ilustrado", "Aditya Y.Bhargava", "2017", "Disponível"});
+                livros.add(new String[]{"6", "Arquitetura Limpa", "Robert C. Martin", "2019", "Emprestado"});
+                livros.add(new String[]{"7", "Introdução à Linguagem SQL ", "Thomas Nield ", "2016", "Emprestado"});
+                livros.add(new String[]{"8", "Modelagem de Dados", "José Osvaldo de Sordi", "2019", "Disponível"});
+                livros.add(new String[]{"9", "Governaça de Dados ", "Carlos Bardieri", "2019", "Disponível"});
+                // Adicionar mais livros conforme necessário
+
+                AcervoDeLivros acervoDeLivrosFrame = new AcervoDeLivros(livros);
+                acervoDeLivrosFrame.setVisible(true);
+            }
+        });
+
+        // Adiciona os menus à barra de menu
         barramenu.add(fileMenu);
         barramenu.add(aboutMenu);
-        
-     // Adiciona os itens de menu ao menu 
+        barramenu.add(collectioMenu);
+
+        // Adiciona os itens de menu ao menu
         fileMenu.add(bibliotecario);
         fileMenu.add(membro);
         fileMenu.add(administrador);
         fileMenu.addSeparator(); // Adiciona uma linha separadora
         fileMenu.add(sair);
-        
-     
-       
-        
-      
-        
-        
-	}
+        collectioMenu.add(acervoDeLivros); // Adicionar o item de menu Acervo de Livros ao collectioMenu
 
+        // Define a barra de menu para este painel
+        setLayout(new BorderLayout());
+        add(barramenu, BorderLayout.NORTH);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Sistema Bibliotecário");
+        UpdateMenu panel = new UpdateMenu();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500, 800);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
